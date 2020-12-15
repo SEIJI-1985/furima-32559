915ece10ea105cc6bca23040509810994b871d2b
SEIJI-1985 <miluku.momo.1985@icloud.com>
2020-12-15 17:02:27 +0900

    Active Storageの導入とバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :image
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :shipping_id
+    validates :send_out_id
+    validates :community_id
+    validates :explanation
+  end
 end
