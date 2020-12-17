941f13dde3b079cef982f302e2e8b6973be1464f
SEIJI-1985 <miluku.momo.1985@icloud.com>
2020-12-17 12:41:48 +0900

    再度、修正箇所

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
     validates :explanation
   end
 
-  validates :price, presence: true, format: { with: /\A[0-9a-zA-Z]+\z/i },
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/i },
                     numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
   
   with_options numericality: { other_than: 1 } do
