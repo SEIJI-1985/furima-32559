59388469aab00554db75b76a65a180b213625320
SEIJI-1985 <miluku.momo.1985@icloud.com>
2020-12-18 15:44:52 +0900

    修正箇所

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order(updated_at: :desc)
