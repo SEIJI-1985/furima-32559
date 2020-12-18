b20be323acabab36324ea7576bf386c383e5f1b1
SEIJI-1985 <miluku.momo.1985@icloud.com>
2020-12-18 14:58:07 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(updated_at: :desc)
@@ -19,7 +20,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+    redirect_to action: :index unless @item.user_id == current_user.id
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -27,6 +39,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:title, :price, :category_id, :condition_id, :shipping_id, :send_out_id, :community_id,
                                  :explanation, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
